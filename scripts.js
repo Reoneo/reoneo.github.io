3430567d64b15ba4cda1911005b3f24d8599e038
Reoneo <reononline@gmail.com>
2024-05-08 21:44:12 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,6 @@
-// Import statements or script tags are necessary depending on your setup
-
-const providerOptions = {
-    walletconnect: {
-        package: WalletConnectProvider, // the WalletConnectProvider variable needs to be defined if this line errors
-        options: {
-            infuraId: "YOUR_INFURA_ID" // Replace this with your Infura Project ID
-        }
+window.onclick = function(event) {
+    var modal = document.getElementById('login-modal');
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-};
-
-const web3Modal = new Web3Modal({
-    network: "mainnet", // Optional
-    cacheProvider: true, // Recommended to turn off for development
-    providerOptions // necessary
-});
-
-async function connectWallet() {
-    try {
-        const provider = await web3Modal.connect();
-        const web3 = new Web3(provider);
-
-        provider.on("accountsChanged", (accounts) => {
-            document.getElementById("connectWallet").innerText = `Connected: ${accounts[0].substring(0, 6)}...${accounts[0].substring(38)}`;
-        });
-
-        provider.on("chainChanged", (chainId) => {
-            window.location.reload();
-        });
-
-        provider.on("disconnect", (error) =>
+}
