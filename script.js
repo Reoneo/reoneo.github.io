007025269cc18b5eaadd115fecef35a7e4541362
Reoneo <reononline@gmail.com>
2024-05-20 00:37:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -113,6 +113,8 @@ window.addEventListener('load', function () {
         const menuButton = document.getElementById('menu-button');
         const menuOverlay = document.getElementById('menu-overlay');
 
+        menuButton.style.opacity = '1'; // Make the menu button visible
+
         menuButton.addEventListener('click', function () {
             menuButton.classList.toggle('active');
             menuOverlay.classList.toggle('active');
